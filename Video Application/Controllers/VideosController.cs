0e8d9fb6f3f64e30a538dfbf8d72c75c36ca4ee6
Jeff Jiang <jeff.jiang098@gmail.com>
2024-07-20 21:04:54 +1000

    Add initial files for ASP.NET Core 6 MVC web application.

---
+++ Video Application/Controllers/VideosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Video_Application.Models;
+
+namespace Video_Application.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VideosController : Controller
+    {
+        private readonly ILogger<VideosController> _logger;
+
+        public VideosController(ILogger<VideosController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Get single video with id 
+        [HttpGet("{id}")]
+        public IActionResult GetVideo() {
+        
+        }
+        // Get all viedos
+        [HttpGet]
+        public IActionResult GetVideos()
+        {
+
+        }
+
+        // Post video
+        [HttpPost]
+        public IActionResult CreateVideo()
+        {
+
+        }
+    
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
